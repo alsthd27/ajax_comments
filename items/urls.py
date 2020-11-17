dedee599ea73fc9262fcd634ad0a5f0212e07da7
Yomin Song <alsthd27@naver.com>
2020-11-17 21:44:14 +0900

    ajax comments

---
+++ items/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name="items"
+urlpatterns = [
+    path('', views.main, name="main"),
+    path('new/', views.new, name="new"),
+    path('create/', views.create, name="create"),
+    path('show/<int:post_id>/', views.show, name="show"),
+    path('delete/<int:post_id>/', views.delete, name="delete"),
+    path('like_toggle/<int:post_id>/', views.like_toggle, name="like_toggle"),
+    path('dislike_toggle/<int:post_id>/', views.dislike_toggle, name="dislike_toggle"),
+    path('create_comment/<int:post_id>/', views.create_comment, name="create_comment"),
+    path('delete_comment/<int:comment_id>/', views.delete_comment, name="delete_comment"),
+]
---
+++ items/views.py
@@ -0,0 +1,105 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import *
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+import json
+from django.template.loader import render_to_string
+
+def main(request):
+    items = Post.objects.all()
+    return render(request, 'items/home.html', {'items':items})
+
+def new(request):
+    return render(request, 'items/new.html')
+
+def create(request):
+    if request.method=="POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        image = request.FILES.get('image')
+        user = request.user
+        Post.objects.create(title=title, content=content, image=image,user=user)
+    return redirect('main')
+
+def show(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.view_count = post.view_count+1
+    post.save()
+    user = request.user
+    context = {
+    'post':post,
+    'user':user,
+    'comments': post.comments.all().order_by('-created_at')
+    }
+    return render(request, 'items/show.html', context)
+
+
+#삭제하기
+def delete(request,post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.delete()
+    return redirect('main')
+
+@login_required
+@require_POST
+def like_toggle(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post_like, post_like_created = Like.objects.get_or_create(user=request.user, post=post)
+    # (object, created)
+
+    if not post_like_created:
+        post_like.delete()
+        result = "like_cancel"
+    else:
+        result = "like"
+    
+    context = {
+        "like_count":post.like_count,
+        "result":result
+    }
+
+    return HttpResponse(json.dumps(context), content_type="application/json")
+
+@login_required
+@require_POST
+def dislike_toggle(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post_dislike, post_dislike_created = Dislike.objects.get_or_create(user=request.user, post=post)
+    # (object, created)
+
+    if not post_dislike_created:
+        post_dislike.delete()
+        result = "dislike_cancel"
+    else:
+        result = "dislike"
+    
+    context = {
+        "dislike_count":post.dislike_count,
+        "result":result
+    }
+
+    return HttpResponse(json.dumps(context), content_type="application/json")
+
+@login_required
+@require_POST
+def create_comment(request, post_id):
+    user = request.user
+    post = get_object_or_404(Post, pk=post_id)
+    content = request.POST.get('content')
+    comment = Comment.objects.create(writer=user, post=post, content=content)
+    rendered = render_to_string('comments/_comment.html', { 'comment': comment, 'user': request.user})
+    context = {
+        'comment' : rendered
+    }
+    return HttpResponse(json.dumps(context), content_type="application/json")
+
+def delete_comment(request, comment_id):
+    user = request.user
+    comment = get_object_or_404(Comment, pk=comment_id)
+    rendered = render_to_string('comments/_comment.html', { 'comment' : comment, 'user':request.user})
+    comment.delete()
+    context={
+        'comment' : rendered
+    }
+    return HttpResponse(json.dumps(context), content_type="application/json")
